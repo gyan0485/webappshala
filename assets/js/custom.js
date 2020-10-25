92dcd6a2dc24f227e48cd3bacd1dd49df14431dd
gyan prakash verma <gyan.prakash@jogaapp.com>
2020-10-25 22:35:10 +0530

    fixes on prixe slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -59,6 +59,35 @@ $(document).ready(function(){
 		duration: 4000
 	});
 
+
+	$('.center').slick({
+		centerMode: true,
+		centerPadding: '0',
+		infinite:false,
+		slidesToShow: 3,
+		responsive: [
+		{
+			// breakpoint: 768,
+			// 	settings: {
+			// 		arrows: false,
+			// 		centerMode: true,
+			// 		centerPadding: '40px',
+			// 		slidesToShow: 3
+			// 	}
+			// },
+			// {
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '0',
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+		
+
 });
 
 // Partical JS
